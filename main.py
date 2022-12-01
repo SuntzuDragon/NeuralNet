70ef6eec5adc91c08290ebe2b99e6f6cdd36283e
SuntzuDragon <imcb01@gmail.com>
2022-11-30 17:23:53 -0700

    Added cross entropy loss function

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ class ReLU:
     def activate(self, inputs):
         return np.maximum(0, inputs)
 
+class CrossEntropyLoss:
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+
+        return -np.log(correct_confidences)
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        return np.mean(sample_losses)
 
 class Layer:
     def __init__(self, n_inputs, n_neurons):
